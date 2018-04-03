6ffef771adc53a996c056af69d1f9c4359db5b24
TiinaEn <tiina.enroth@outlook.com>
2018-04-03 09:18:56 +0300

    projektin alustus

---
+++ src/main/java/fi/academy/loppuprojekti_rest/LoppuprojektiRestApplication.java
@@ -0,0 +1,12 @@
+package fi.academy.loppuprojekti_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoppuprojektiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoppuprojektiRestApplication.class, args);
+	}
+}
