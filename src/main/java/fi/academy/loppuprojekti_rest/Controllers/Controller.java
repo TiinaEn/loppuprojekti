4956fa62d40fc7c2a95da633d339a9aeb66c13e0
TiinaEn <tiina.enroth@outlook.com>
2018-04-03 17:02:16 +0300

    controlleriin lisätty deletemapping ja yritetty lisätä hakutoiminto. Repoon lisätty uusi kysely nimen perusteella hakemisesta

--- src/main/java/fi/academy/loppuprojekti_rest/Controllers/Controller.java
+++ src/main/java/fi/academy/loppuprojekti_rest/Controllers/Controller.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/travelapp")
@@ -14,18 +15,30 @@ public class Controller {
 @Autowired
     private DestinationRepo dr;
 
-
 @GetMapping ("/destinations")
     public Iterable<Destination> findDestinations() {
         Iterable<Destination> iteDestination = dr.findAll();
         return iteDestination;
     }
-
 @PostMapping ("/create")
     public ResponseEntity<?> createDestination (@RequestBody Destination destination) {
     Destination saved = dr.save(destination);
     String  address = "http://localhost:8080/create/"+saved.getId();
     return ResponseEntity.created(URI.create(address)).build();
+}
+@DeleteMapping("/remove/{id}")
+    public ResponseEntity<?> removeDestination(@PathVariable Integer id, @RequestBody Destination destination) {
+    destination.setId(id);
+    dr.delete(destination);
+    return ResponseEntity.noContent().build();
+}
+@GetMapping ("/find/{name}")
+    public Destination findByName(@PathVariable("name") String name) {
+    Optional<Destination> optDest = dr.findByName(name);
+if (optDest.isPresent())
+    return optDest.get();
+    return null;
+
 }
 
 
--- src/main/java/fi/academy/loppuprojekti_rest/Repositories/DestinationRepo.java
+++ src/main/java/fi/academy/loppuprojekti_rest/Repositories/DestinationRepo.java
@@ -3,5 +3,8 @@ package fi.academy.loppuprojekti_rest.Repositories;
 import fi.academy.loppuprojekti_rest.Entities.Destination;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface DestinationRepo extends CrudRepository <Destination, Integer> {
+    Optional<Destination> findByName(String name);
 }
