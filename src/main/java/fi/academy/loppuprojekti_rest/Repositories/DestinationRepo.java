7b2a979f67bbfa7192e8198046444a1bf93b7da0
MillaPeltola <milla.peltola@icloud.com>
2018-04-05 14:57:42 +0300

    upeita queryja

--- src/main/java/fi/academy/loppuprojekti_rest/Repositories/DestinationRepo.java
+++ src/main/java/fi/academy/loppuprojekti_rest/Repositories/DestinationRepo.java
@@ -11,11 +11,29 @@ import java.util.Optional;
 
 public interface DestinationRepo extends CrudRepository <Destination, Integer> {
     Optional<Destination> findByName(String name);
+
     Iterable<Destination> findByNameContains(String name);
 
     @Query
-    ("SELECT d FROM Destination d where (d.country like concat ('%', :searchword, '%') " +
-            "or d.city like concat('%', :searchword, '%') or d.name like concat('%', :searchword, '%')" +
-            "or d.category like concat('%', :searchword, '%')) and d.user = :user")
+            ("SELECT d FROM Destination d where (d.country like concat ('%', :searchword, '%') " +
+                    "or d.city like concat('%', :searchword, '%') or d.name like concat('%', :searchword, '%')" +
+                    "or d.category like concat('%', :searchword, '%')) and d.user = :user")
     Iterable<Destination> findBySearchWord(@Param("searchword") String word, @Param("user") User user);
+
+    @Query
+            ("SELECT d.country FROM Destination d where d.user = :user")
+    Iterable<Destination> findCountries(@Param("user") User user);
+
+    @Query
+            ("SELECT d.city FROM Destination d where d.country = :country and d.user = :user")
+    Iterable<Destination> findCitiesByCountry(@Param("country") String country, @Param("user") User user);
+
+    @Query
+            ("SELECT d.name FROM Destination d where d.country = :country and d.city = :city and d.user = :user")
+    Iterable<Destination> findDestinationsByCity(@Param("country") String country, @Param("city") String city, @Param("user") User user);
+
+    @Query
+            ("SELECT d FROM Destination d where d.country = :country and d.city = :city and d.name = :name and d.user = :user")
+    Iterable<Destination> findDestination(@Param("country") String country, @Param("city") String city, @Param("name") String name, @Param("user") User user);
+
 }
