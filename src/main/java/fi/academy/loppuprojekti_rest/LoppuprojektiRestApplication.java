eaab1afbbf436eff6d2a09b0f694242d3f38ec57
MillaPeltola <milla.peltola@icloud.com>
2018-04-10 15:55:09 +0300

    Kaikki Heidin nimiin

--- src/main/java/fi/academy/loppuprojekti_rest/LoppuprojektiRestApplication.java
+++ src/main/java/fi/academy/loppuprojekti_rest/LoppuprojektiRestApplication.java
@@ -24,9 +24,9 @@ public class LoppuprojektiRestApplication {
 			ur.save(t);
 			User h = new User("Heidi", "Heidi", "heidi@heidi.fi", "basic", 1, " ");
 			ur.save(h);
-			Destination r = new Destination("restaurant", "Chinatown",  "China", "Beijing", "Great food, cheap and tasty", t);
+			Destination r = new Destination("restaurant", "Chinatown",  "China", "Beijing", "Great food, cheap and tasty", h);
 			dr.save(r);
-			dr.save(new Destination("restaurant", "JavaHut",  "Belgium", "Brussels", "Best coffee in town, free wifi", t));
+			dr.save(new Destination("restaurant", "JavaHut",  "Belgium", "Brussels", "Best coffee in town, free wifi", h));
 			dr.save(new Destination("restaurant", "Ikea",  "Finland", "Vantaa", "Cheap meatballs", h));
 			dr.save(new Destination("restaurant", "Rosso",  "Finland", "Helsinki", "Pizza and pasta", h));
             dr.save(new Destination("restaurant", "Farang",  "Finland", "Helsinki", "Expensive Asian meatballs", h));
